b64495010d0673718c89a989820f75b52ee65251
Temur5319436 <temur5319436@gmail.com>
2024-02-14 20:19:58 +0500

    modified: biz index.php ni o'zgartirdik

--- includes/header.php
+++ includes/header.php
@@ -5,15 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="boshsahifa/menu1.css">
-    <title> <?php echo $title ?? 'Eror?';  ?></title>
-   
+
+    <title> <?php echo $title ?? 'Error'; ?></title>
+
 </head>
 
-<body>
-
-    <header data-bs-theme="dark">
-        <?php require 'navbar.php'; ?>
-    </header>
-
-    <main> 
+<header data-bs-theme="dark">
+    <?php require 'navbar.php'; ?>
+</header>
